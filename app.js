494f84d2247c34b51e8261e51dad3020676e1bad
Prakhar2100 <prakhar95985556@gmail.com>
2023-05-10 16:28:53 +0530

    FIX_UI

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 var express                 = require("express"),
     mongoose                = require("mongoose"),
